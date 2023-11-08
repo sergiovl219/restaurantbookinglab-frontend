b6feedcbeb166e4bcf71a67e23baead58cf88718
Sergio Vargas <sergiovl219@gmail.com>
2023-11-07 21:55:11 -0600

    Simple Login Component

---
+++ src/components/Login.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Login: React.FC = () => {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!username || !password) {
+            setError('Please fill all the fields.');
+        } else {
+            console.log('Iniciando sesión con email:', username, 'y contraseña:', password);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input type="email" value={username} onChange={(e) => setUserName(e.target.value)} />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                {error && <div className="error">{error}</div>}
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
