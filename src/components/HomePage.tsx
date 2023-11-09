d681ec8571bcf473f8c8a6db59e5909346452b39
Sergio Vargas <sergiovl219@gmail.com>
2023-11-09 13:21:28 -0600

    Visual bug on selected restaurant.

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,29 +1,40 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectUserInfo } from '../store/slices/userInfoSlice';
-import {selectRestaurantID, setRestaurantID, setRestaurantURL} from '../store/slices/restaurantSlice';
+import {
+    selectRestaurantID,
+    selectRestaurantName,
+    setRestaurantID,
+    setRestaurantURL,
+    setRestaurantName,
+} from '../store/slices/restaurantSlice';
 
 const HomePage: React.FC = () => {
     const userInfo = useSelector(selectUserInfo);
     const restaurantId = useSelector(selectRestaurantID);
+    const restaurantName = useSelector(selectRestaurantName);
     const dispatch = useDispatch();
 
     useEffect(() => {
         if (userInfo && userInfo.restaurants_info.length > 0) {
             dispatch(setRestaurantID(userInfo.restaurants_info[0].id));
             dispatch(setRestaurantURL(userInfo.restaurants_info[0].page_url));
+            dispatch(setRestaurantName(userInfo.restaurants_info[0].name));
         }
     }, [userInfo, dispatch]);
 
-    const handleRestaurantChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const handleRestaurantChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
         const selectedRestaurantId = event.target.value;
         const selectedRestaurant = userInfo.restaurants_info.find(
-            restaurant => restaurant.id === selectedRestaurantId
+            (restaurant) => restaurant.id === selectedRestaurantId
         );
 
         if (selectedRestaurant) {
             dispatch(setRestaurantID(selectedRestaurantId));
             dispatch(setRestaurantURL(selectedRestaurant.page_url));
+            dispatch(setRestaurantName(selectedRestaurant.name));
         }
     };
 
@@ -32,7 +43,7 @@ const HomePage: React.FC = () => {
             <header>
                 {userInfo && userInfo.restaurants_info.length > 0 && (
                     <div>
-                        <h1>Welcome to {userInfo.restaurants_info[0].name} Restaurant</h1>
+                        <h1>Welcome to {restaurantName} Restaurant</h1>
                         <label>Select a Restaurant:</label>
                         <select value={restaurantId} onChange={handleRestaurantChange}>
                             {userInfo.restaurants_info.map((restaurant) => (
--- src/store/slices/restaurantSlice.ts
+++ src/store/slices/restaurantSlice.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface RestaurantState {
     restaurantId: string;
     restaurantURL: string | null;
+    restaurantName: string;
 }
 
 const initialState: RestaurantState = {
     restaurantId: "None Selected",
     restaurantURL: null,
+    restaurantName: "",
 };
 
 const restaurantSlice = createSlice({
@@ -20,11 +22,19 @@ const restaurantSlice = createSlice({
         setRestaurantURL: (state, action: PayloadAction<string | null>) => {
             state.restaurantURL = action.payload;
         },
+        setRestaurantName: (state, action: PayloadAction<string>) => {
+            state.restaurantName = action.payload;
+        },
     },
 });
 
-export const { setRestaurantID, setRestaurantURL } = restaurantSlice.actions;
+export const {
+    setRestaurantID,
+    setRestaurantURL,
+    setRestaurantName
+} = restaurantSlice.actions;
 export const selectRestaurantID = (state: { restaurant: RestaurantState }) => state.restaurant.restaurantId;
 export const selectRestaurantURL = (state: { restaurant: RestaurantState }) => state.restaurant.restaurantURL;
+export const selectRestaurantName = (state: { restaurant: RestaurantState }) => state.restaurant.restaurantName;
 
 export default restaurantSlice.reducer;
